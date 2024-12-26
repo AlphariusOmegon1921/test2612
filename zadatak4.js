d0d16267eb1fee84d0b80cb885e0d06867e8a58f
Tarik Carkadzic <tarik.carkadzic@gmail.com>
2024-12-26 15:59:23 +0100

    zadatak 4

---
+++ zadatak4.js
@@ -0,0 +1,22 @@
+/*Ulaz je string koji se sastoji od riječi i razmaka. Vratiti dužinu posljednje riječi u stringu.
+
+Riječ je maksimalni podstring koji se sastoji samo od znakova koji nisu razmaci.
+
+Primjer 1:
+
+Ulaz: str = "Hello World"
+Izlaz: 5
+Objašnjenje: Posljednja riječ je "World" čija je dužina 5
+*/
+
+let string = "fly me to the moon  ";
+
+function duzinaZadnjegStringa(string) {
+  const rijeci = string
+    .trim()
+    .split(" ")
+    .filter((word) => word !== "");
+
+  return rijeci[rijeci.length - 1].length;
+}
+console.log(duzinaZadnjegStringa(string));
